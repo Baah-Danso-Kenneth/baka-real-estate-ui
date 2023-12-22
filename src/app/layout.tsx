1ee15f2663369548b1082efd7f44b5dce20db21d
Kenneth Danso <kwadwobaahdanso@gmail.com>
2023-12-22 23:49:53 +0000

    structured the layout for project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Head from 'next/head'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import SocialNavigation from '@/components/SocialNavigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -25,6 +26,7 @@ export default function RootLayout({
       </Head>
       <Header/>
       <body className={inter.className}>{children}</body>
+      <SocialNavigation/>
       <Footer/>
     </>
   )
